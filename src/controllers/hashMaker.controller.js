dcbd4a772f623a011cc62227880cd53117183ebd
ahann <ahannshi@gmail.com>
2023-03-14 10:58:44 +0700

    align response

--- src/controllers/hashMaker.controller.js
+++ src/controllers/hashMaker.controller.js
@@ -1,18 +1,20 @@
 import bcrypt from 'bcrypt';
 
 async function generate(req, res) {
-    try {
-        const result = await bcrypt.hash(req.body.plainText, 15);
-        res.status(200).json({
-            plainText:req.body.plainText,
-            result: result
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            msg: "Internal Server Error"
-        });
-    }
+  try {
+    const result = await bcrypt.hash(req.body.plainText, 15);
+    res.status(200).json({
+      data: {
+        plainText: req.body.plainText,
+        result: result,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      msg: "Internal Server Error",
+    });
+  }
 }
 
 export default { generate };
